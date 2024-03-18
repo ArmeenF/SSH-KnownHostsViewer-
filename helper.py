de3a520f7bf01b08a683472c6113ea7d0f7a7078
Armeen Farange <47340511+ArmeenF@users.noreply.github.com>
2024-03-17 17:07:02 -0700

    Official repo initialization

---
+++ helper.py
@@ -0,0 +1,152 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=unnecessary-dunder-call
+# pylint: disable=broad-exception-caught
+
+
+"""
+Library/Component: HostKeyViewer
+Copyright: © 2024 ArmeenF
+Description: This file contains the helper functions for the Known Hosts Viewer application.
+Author: ArmeenF
+Documentation: For further documentation and usage examples, please refer to the README file.
+"""
+
+import os
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from text_vault import documentation_text
+
+
+def restart_app(instance):
+    instance.root.destroy()  # Close current window
+    instance.root = tk.Tk()  # Create a new one
+    instance.__init__(instance.root)  # Reinitialize the application
+
+
+def open_known_hosts(tree):
+    filepath = filedialog.askopenfilename(
+        initialdir=os.path.expanduser("~/.ssh"),
+        defaultextension=".txt",
+        filetypes=[("All files", "*.*")],
+    )
+    if not filepath:
+        return
+
+    try:
+        with open(filepath, "r", encoding="utf-8") as file:
+            # Clear current content
+            for row in tree.get_children():
+                tree.delete(row)
+
+            for line in file:
+                parts = line.strip().split(" ", 2)
+                if len(parts) >= 2:
+                    ip, key = parts[0], " ".join(parts[1:])
+                    tree.insert("", tk.END, values=("", ip, key), tags=("unchecked",))
+    except FileNotFoundError as e:
+        messagebox.showerror("Error", f"File not found: {str(e)}")
+    except IOError as e:
+        messagebox.showerror("Error", f"IO error occurred: {str(e)}")
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred: {str(e)}")
+
+
+def toggle_checkbox(tree, event):
+    item = tree.identify_row(event.y)
+    if not item:
+        return
+
+    if "unchecked" in tree.item(item, "tags"):
+        tree.item(item, tags=("checked", "highlight"))
+        update_visual_marker(tree, item, action="check")
+    else:
+        tree.item(item, tags=("unchecked",))
+        update_visual_marker(tree, item, action="uncheck")
+
+
+def update_visual_marker(tree, item, action="check"):
+    if action == "check":
+        # Update the first value in the `values` tuple with a checkmark (or any other marker).
+        tree.item(item, values=("✔", *tree.item(item)["values"][1:]))
+    else:
+        # Remove the checkmark
+        tree.item(item, values=("", *tree.item(item)["values"][1:]))
+
+
+def about_dialog(parent):
+    about_window = tk.Toplevel(parent)
+    about_window.title("About Known Hosts Viewer")
+    about_window.geometry("300x150")
+
+    about_text = "Known Hosts Viewer\nVersion 1.0\n© 2024 ArmeenF"
+    tk.Label(about_window, text=about_text, justify=tk.CENTER, padx=10, pady=10).pack()
+
+    tk.Button(
+        about_window,
+        text="Close",
+        command=about_window.destroy,
+        background="#E1E1E1",
+        relief="raised",
+    ).pack(pady=10)
+
+
+def documentation_dialog(parent):
+    documentation_window = tk.Toplevel(parent)
+    documentation_window.title("Documentation")
+    documentation_window.geometry("500x800")  # Adjust size as needed
+
+    # Create a scrollbar
+    scrollbar = tk.Scrollbar(documentation_window)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+    # Create a text widget with a scrollbar
+    text_widget = tk.Text(
+        documentation_window,
+        wrap=tk.WORD,
+        yscrollcommand=scrollbar.set,
+        padx=10,
+        pady=10,
+    )
+    text_widget.pack(expand=True, fill=tk.BOTH)
+
+    # Configure the scrollbar
+    scrollbar.config(command=text_widget.yview)
+
+    # Insert the documentation text into the text widget and disable editing
+    text_widget.insert(tk.END, documentation_text.strip())
+    text_widget.config(state=tk.DISABLED)
+
+    # Add a button to close the dialog
+    close_button = tk.Button(
+        documentation_window,
+        text="Close",
+        command=documentation_window.destroy,
+        background="#E1E1E1",
+        relief="raised",
+    )
+    close_button.pack(pady=10)
+
+
+def delete_selected_rows(tree):
+    selected_items = tree.selection()
+    print("Selected items before deletion:", selected_items)  # Debugging line
+    selected_ips = [tree.item(item, "values")[1] for item in selected_items]
+
+    # First, make a list of all items to delete to avoid any modification issues during iteration
+    items_to_delete = list(selected_items)
+
+    # Remove selected items from GUI
+    for item in items_to_delete:
+        tree.delete(item)
+
+    # Update the known_hosts file
+    known_hosts_path = os.path.expanduser("~/.ssh/known_hosts")
+    with open(known_hosts_path, "r", encoding="utf-8") as file:
+        lines = file.readlines()
+
+    with open(known_hosts_path, "w", encoding="utf-8") as file:
+        for line in lines:
+            if not any(ip in line for ip in selected_ips):
+                file.write(line)
---
+++ known_hosts_gui.py
@@ -0,0 +1,133 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+"""
+Library/Component: HostKeyViewer
+Copyright: © 2024 ArmeenF
+Description: This file contains the implementation of the KnownHostsViewer class.
+Original Author: ArmeenF
+Documentation: For further documentation and usage examples, please refer to the README file.
+Usage Examples: [Provide usage examples if applicable]
+"""
+
+import tkinter as tk
+import tkinter.font as tkFont
+from tkinter import ttk
+from helper import (
+    restart_app,
+    open_known_hosts,
+    about_dialog,
+    documentation_dialog,
+    toggle_checkbox,
+    delete_selected_rows,
+)
+
+
+class KnownHostsViewer:
+    def __init__(self, root_param):
+        self.root = root_param
+        self.configure_root()
+        self.create_menu()
+        self.configure_style()
+        self.setup_bottom_frame()
+        self.setup_treeview()
+        self.setup_scrollbar()
+
+    def configure_root(self):
+        self.root.title("Known Hosts Viewer")
+        self.root.geometry("800x600")
+        self.root.option_add("*Menu.font", ("JetBrains Mono", 10))
+
+    def create_menu(self):
+        self.menu = tk.Menu(self.root)
+        self.root.config(menu=self.menu)
+
+        self.file_menu = tk.Menu(self.menu, tearoff=0)
+        self.menu.add_cascade(label="File", menu=self.file_menu)
+        self.file_menu.add_command(
+            label="Open", command=lambda: open_known_hosts(self.tree)
+        )
+        self.file_menu.add_command(label="Restart", command=lambda: restart_app(self))
+        self.file_menu.add_separator()
+        self.file_menu.add_command(
+            label="About", command=lambda: about_dialog(self.root)
+        )
+        self.file_menu.add_command(
+            label="Documentation", command=lambda: documentation_dialog(self.root)
+        )
+        self.file_menu.add_separator()
+        self.file_menu.add_command(label="Exit", command=self.root.quit)
+
+    def configure_style(self):
+        style = ttk.Style()
+        style.configure(
+            "Treeview.Heading",
+            font=("JetBrains Mono Bold", 10),
+            background="#E1E1E1",
+            relief="raised",
+        )
+        style.map(
+            "Treeview",
+            background=[("selected", "lightgray")],
+            foreground=[("selected", "black")],
+        )
+
+    def setup_bottom_frame(self):
+        bottom_frame = tk.Frame(self.root)
+        bottom_frame.pack(side=tk.BOTTOM, fill=tk.X, expand=False)
+
+        # Add and style the button
+        styled_button = tk.Button(
+            bottom_frame,
+            text="Delete Selected",
+            command=lambda: delete_selected_rows(self.tree),
+            bg="red",
+            highlightthickness=10,
+            bd=6,
+        )
+        styled_button.pack(side=tk.RIGHT, padx=10, pady=5)
+
+    def delete_button_action(self):
+        # Placeholder for your button's functionality
+        print("New Bottom Button Clicked!")
+
+    def setup_treeview(self):
+        self.tree = ttk.Treeview(
+            self.root,
+            columns=("Select", "IP", "Key"),
+            show="headings",
+            selectmode="extended",
+        )
+
+        self.tree.heading("#1", text="Select", anchor=tk.W)
+        self.tree.heading("IP", text="IP Address", anchor=tk.W)
+        self.tree.heading("Key", text="SSH Key", anchor=tk.W)
+        self.tree.column("#1", width=60, stretch=tk.NO)
+        self.tree.column("IP", width=150, stretch=tk.NO)
+        self.tree.column("Key", stretch=tk.YES)
+        self.tree.bind("<Button-1>", lambda event: toggle_checkbox(self.tree, event))
+        self.tree.pack(fill=tk.BOTH, expand=1)
+
+    def setup_scrollbar(self):
+        self.scrollbar = ttk.Scrollbar(
+            self.root, orient="vertical", command=self.tree.yview
+        )
+        self.tree.configure(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    # -------------------------------------------------------- #
+    # Define the default font for all widgets
+    default_font = tkFont.nametofont("TkDefaultFont")
+    default_font.configure(family="JetBrains Mono Bold", size=12)
+    # -------------------------------------------------------- #
+    # Define the default icon and title for the application
+    img = tk.PhotoImage(file=r"Image\ssh.png")
+    root.iconphoto(False, img)
+    root.title("SSH Host Key Viewer")
+    # -------------------------------------------------------- #
+    viewer = KnownHostsViewer(root)
+    root.mainloop()
